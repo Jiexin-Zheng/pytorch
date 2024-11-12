eb1d65f12b331c3da3a00232a599b07bcbaa1e79
Ding, Yi1 <yi1.ding@intel.com>
2024-11-12 09:41:20 +0000

    Add OneDNN Graph SDPA

--- aten/src/ATen/native/mkldnn/xpu/detail/oneDNN.h
+++ aten/src/ATen/native/mkldnn/xpu/detail/oneDNN.h
@@ -107,4 +107,20 @@ dnnl::memory::dims deconv_dst_size(
     IntArrayRef dst_padding,
     int64_t groups);
 
+namespace graph {
+using namespace dnnl::graph;
+
+TORCH_API void gpu_float_sdpa(
+    int batch_size,
+    int seq_len_q,
+    int seq_len_k,
+    int num_head,
+    int size_per_head,
+    const Tensor& query,
+    const Tensor& key,
+    const Tensor& value,
+    const Tensor& attn_mask,
+    const float& softmax_scale,
+    const Tensor& output);
+} // namespace graph
 } // namespace at::native::onednn
